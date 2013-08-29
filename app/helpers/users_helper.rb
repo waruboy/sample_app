1937e548da03c164f1be45d6ce57152187735e4f
Taufiq Muhammadi <waruboy@yahoo.com>
2013-08-29 12:31:18 +0700

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
-end
+
+	# Returns the Gravatar (http://gravatar.com/)for the give user
+	def gravatar_for(user, options = { size: 50})
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+end 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define  do 
+	factory :user do 
+		name		"Taufiq Muhammadi"
+		email		"taufiqm@examle.com"
+		password	"foobar"
+		password_confirmation "foobar"
+	end
+end
